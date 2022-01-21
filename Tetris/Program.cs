d78e71a8618f667f3caa0a8a4ec5c76269d60534
KungSeung <nkhone123456@naver.com>
2022-01-21 10:33:26 +0900

    Tetris

---
+++ Tetris/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+enum TBlock
+{
+	WALL,	//0
+	VOID,	//1 , 값을 넣어주고 싶으면 Jeju = 5
+	BLOCK,
+}
+
+public class Test
+{
+
+}
+
+namespace Tetris
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			TScreen NewSC = new TScreen(10, 15);
+
+			Block NewBlcok = new Block();
+
+			for (int y = 0; y < 3; ++y)
+			{
+				for (int x = 0; x < 3; ++x)
+				{
+					NewSC.SetBlock(y, x, TBlock.WALL);
+				}
+			}
+
+			//NewSC.SetBlock(3, 3, TBlock.WALL);
+
+			NewSC.Render();
+			
+			Console.ReadKey();
+			
+		}
+	}
+}
---
+++ Tetris/TScreen.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class TScreen
+{
+	List<List<TBlock>> BlockList = new List<List<TBlock>>();
+
+	public void SetBlock(int _y, int _x, TBlock _Type)
+	{
+		BlockList[_y][_x] = _Type;
+	}
+
+	public void Render()
+	{
+		for (int y = 0; y < BlockList.Count; ++y)
+		{
+			for (int x = 0; x < BlockList[y].Count; ++x)
+			{
+				switch (BlockList[y][x])
+				{
+					case TBlock.WALL:
+						Console.Write("▣");
+						break;
+					case TBlock.VOID:
+						Console.Write("□");
+						break;
+					case TBlock.BLOCK:
+						Console.Write("■");
+						break;
+					default:
+						break;
+				}
+			}
+			Console.WriteLine();
+		}
+	}
+
+	public void TetrissScreen(int _X, int _Y)
+	{
+		for (int y = 0; y < _Y; y++)
+		{
+			BlockList.Add(new List<TBlock>());
+			for (int x = 0; x < _X; ++x)
+			{
+				BlockList[y].Add(TBlock.VOID);
+			}
+		}
+
+		//첫번째 줄
+		for (int i = 0; i < BlockList[0].Count; i++)
+		{
+			BlockList[0][i] = TBlock.WALL;
+		}
+		//마지막 줄
+		for (int i = 0; i < BlockList[BlockList.Count - 1].Count; i++)
+		{
+			BlockList[BlockList.Count - 1][i] = TBlock.WALL;
+		}
+	}
+}
+
